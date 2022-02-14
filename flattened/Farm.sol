9d4e86af5ac08d8799d717e4cc88967705061fca
Esteban Abaroa <90033261+estebanabaroa@users.noreply.github.com>
2022-02-14 05:55:57 +0000

    Update Farm.sol

--- flattened/Farm.sol
+++ flattened/Farm.sol
@@ -796,7 +796,7 @@ contract Farm is Ownable {
         poolInfo[_pid].allocPoint = _allocPoint;
     }
 
-    // reward multiplier _from to _to block.
+    // reward multiplier _from to _to block
     function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
         if (_to <= bonusEndBlock) {
             uint256 timeAdjustedBonusMultiplier = bonusMultiplier.mul(getPercentBonusTimeLeft());
